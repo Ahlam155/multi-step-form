ab453698175ff9725de0e542aeef0d78fbc394ec
Ahlam Ragab <ahlam.ragab2000@gmail.com>
2023-03-22 17:33:25 +0200

    Update shared.service.ts

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Plans } from '../models/plans';
 import { NavigationStart, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class SharedService {
+export class SharedService implements OnDestroy{
   offer:string="Yearly"
   selectedPlan!: Plans;
   selectedOns:Plans[]=[];
@@ -17,8 +17,11 @@ export class SharedService {
   constructor(private router: Router) {
     this.subscription = router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
-        this.browserRefresh = !router.navigated;
+        this.browserRefresh = router.navigated;
       }
   });
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
