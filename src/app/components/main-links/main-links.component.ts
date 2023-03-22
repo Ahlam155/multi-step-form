1893b9cb5ea1ab788ecf436a87968806348a0257
Ahlam Ragab <ahlam.ragab2000@gmail.com>
2023-03-22 16:18:37 +0200

    Update main-links.component.ts

--- src/app/components/main-links/main-links.component.ts
+++ src/app/components/main-links/main-links.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { SharedService } from 'src/app/services/shared.service';
-
 @Component({
   selector: 'app-main-links',
   templateUrl: './main-links.component.html',
   styleUrls: ['./main-links.component.css']
 })
 export class MainLinksComponent implements OnInit {
-page=this.sharedService.page
-validation:boolean=this.sharedService.validation1
-  constructor(private sharedService:SharedService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
